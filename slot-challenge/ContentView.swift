691c3565b6adc018c4bce21709966bfa3a6f7260
Md Mazedul Islam Khan <mazedulislamkhan@gmail.com>
2021-07-10 16:51:07 +0600

    Initial Commit

---
+++ slot-challenge/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  slot-challenge
+//
+//  Created by Md Mazedul Islam Khan on 10/7/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ slot-challenge/slot_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  slot_challengeApp.swift
+//  slot-challenge
+//
+//  Created by Md Mazedul Islam Khan on 10/7/21.
+//
+
+import SwiftUI
+
+@main
+struct slot_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
